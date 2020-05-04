b52d6c22c683557b5b58a3668d3b4fee9e08ade5
Ibsen <ibsenmelo@gmail.com>
2020-05-04 12:55:17 -0300

    primeiro commit

---
+++ R/bota_ponto.R
@@ -0,0 +1,23 @@
+
+
+#' @title
+#'
+#' @description Função criada para auxiliar baixa_domicilio. Serve para inserir ponto que separa inteiro de decimal.
+#'
+#' @param x Caracter que vai receber ponto
+#' @param apos Quantidade de caracter a esquerda do ponto
+#'
+#' @return valor com ponto
+#' @export
+#'
+#' @examples
+#' bota_ponto("123456")
+#' bota_ponto("123456", apos = 2)
+#' bota_ponto("123456", 1)
+bota_ponto <- function(x, apos = 3) {
+  as.numeric(str_c(str_sub(x, end = apos),".",
+                   str_sub(x, start = (apos+1))))
+}
+
+
+
